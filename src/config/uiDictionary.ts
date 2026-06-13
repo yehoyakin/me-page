bb087afb6bde75f2ba22ce525da15201b17b3895
yehoyakin <65365993+yehoyakin@users.noreply.github.com>
2026-06-13 04:09:37 -0400

    portfolio done

--- src/config/uiDictionary.ts
+++ src/config/uiDictionary.ts
@@ -4,24 +4,52 @@ export type UiConfig = {
   transition: "crt" | "grid" | "crash";
   font: "psx" | "title" | "terminal";
   theme: "primary" | "secondary" | "terminal";
+
+  text: {
+    base: string;
+    hover: string;
+  };
 };
 
 export const uiDictionary: Record<UiVariant, UiConfig> = {
   primary: {
     transition: "crt",
     font: "psx",
-    theme: "primary"
+    theme: "primary",
+    text: {
+      base: "text-white",
+      hover: "group-hover:text-amber-400"
+    }
   },
 
   secondary: {
     transition: "grid",
-    font: "title",
-    theme: "secondary"
+    font: "psx",
+    theme: "secondary",
+    text: {
+      base: "text-white",
+      hover: "group-hover:text-cyan-400"
+    }
   },
 
   terminal: {
     transition: "crash",
     font: "terminal",
-    theme: "terminal"
+    theme: "terminal",
+    text: {
+      base: "text-green-200",
+      hover: "group-hover:text-green-400"
+    }
   }
 };
+
+export type ProjectRole =
+  | "lead_programmer"
+  | "developer"
+  | "ui"
+  | "game_design"
+  | "art"
+  | "audio"
+  | "tech_art"
+  | "systems"
+  | "tools";
