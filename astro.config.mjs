e45cc017015614b7d8bce9d7bc975a22d4d9a9d5
yehoyakin <65365993+yehoyakin@users.noreply.github.com>
2026-06-19 15:34:33 -0400

    tailwind new version update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [tailwind()],
   output: "static",
-  site:"https://yehoyakin.github.io",
-  base:"/me-page"
+  site: "https://yehoyakin.github.io",
+  base: "/me-page/",
+
+vite: {
+    plugins: [tailwindcss()], 
+  },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
