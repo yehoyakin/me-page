0a5d714b590728ece40acaff4496f6818db0e705
yehoyakin <65365993+yehoyakin@users.noreply.github.com>
2026-06-19 18:44:27 -0400

    pathing fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ export default defineConfig({
   output: "static",
   site: "https://yehoyakin.github.io",
   base: "/me-page/",
-  trailingSlash: "always", // <-- Forces consistent routing behavior
   vite: {
     plugins: [tailwindcss()],
   },
