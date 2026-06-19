39176ba5fe946368ff39ac3840a7620f05479cde
yehoyakin <65365993+yehoyakin@users.noreply.github.com>
2026-06-19 13:26:33 -0400

    deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static"
+  output: "static",
+  site:"https://yehoyakin.github.io",
+  base:"/cyan-centauri"
 });
