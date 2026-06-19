d4c8713b5c8351a6055a162a2896697d8aae554d
yehoyakin <65365993+yehoyakin@users.noreply.github.com>
2026-06-19 16:40:23 -0400

    activation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "static",
   site: "https://yehoyakin.github.io",
   base: "/me-page/",
-
-vite: {
-    plugins: [tailwindcss()], 
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
